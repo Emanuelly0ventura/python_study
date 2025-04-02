28bdd01fb4e0ae8e6ebf4b71a3b2440934c455fd
manu <haru.noom0@gmail.com>
2025-04-02 20:52:18 -0300

    for,in,range,while

---
+++ Cp2-tudo/sabonete 2.py
@@ -0,0 +1,36 @@
+#for in e range
+for x in range(1, 11, 2):
+    print(x)    #a primeira é o começo a segunda o final e a terceira quantos vc quer pular por numero
+
+
+credit_card = "1234-5678-9012-3456"
+
+#for x in credit_card:
+    #print(x)  
+
+# continue == pular o numero/coisa
+# break == para antes que o nunmero/coisa apareça
+
+for x in range(1,21):
+  if x == 13:
+    break
+  else:
+    print(x)
+
+#While
+
+name = input("digite seu nome: ")
+
+while name == "":
+    print("Você não escreveu o seu nome")
+    name = input("digite seu nome: ")
+print(f"ola {name}") 
+
+comida = input("Escreva uma comida que vc goste (pra sair q): ")
+
+while not comida == "q":
+    print(f"vc gosta de {comida}")
+    comida = input("Escreva outra comida que vc goste (pra sair q): ")
+
+print("tchau :(")
+
--- aula6.py
+++
@@ -1,8 +0,0 @@
-s = 3
-
-print('a soma vale{}',format(s))
-
-n1 = "10"
-
-print(int(n1) + 5)
-
