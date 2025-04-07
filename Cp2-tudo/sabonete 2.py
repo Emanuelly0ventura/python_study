24781c9645b9851a0a39f7ea8ebf634383b1c588
manu <haru.noom0@gmail.com>
2025-04-07 19:49:47 -0300

    revendo a aula de collection

--- Cp2-tudo/sabonete 2.py
+++ Cp2-tudo/sabonete 2.py
@@ -35,14 +35,65 @@ while not comida == "q":
 print("tchau :(")
 
 #aula q eu entendi nada
+
+#list = [] mudavel e coloca ordem - duplicar OK
+#set = {} sem ordem e inalterável - add / remover OK mas duplicar NÂO
+#tuple = () mudavel e inalterável - duplica OK so q mais rapido
+
 frutas = ("maça", "abacaxi", "morango", "banana")
 
 frutas[0]
 
+print(len(frutas))
+print("abacaxi" in frutas)
+
 for i in (0,1,2,3):
     print(frutas[i])
 
+#ESSES SÃO UTILIZADOS EM LIST
+
+#frutas.clear - limpa lista
+#frutas[0] = "pera" - substitui a possição q escolher
+#for futa in frutas:print(futa) - deixa em ordem
+#frutas.insert(0, "pera") - adiciona na frente
+#frutas.append("sopa") - add  
 #frutas.sort --> classifica
 #so se usa se os tipos são iguas
 #frutas.reverse --> inverte
-cordenadas = [(1,2), (3,4), (5,6)]
+#print(len(frutas)) - quantidade de itens
+
+#ESSES SÃO UTILIZADOS EM SET
+
+#set n repete itens
+#frutas.insert(0, "pera") - adiciona na frente
+#frutas.append("sopa") - add  
+#frutas.sort - classifica
+#frutas.add("pera") - adiciona na lista de forma aleatoria(pelo oq eu entendi)
+#frutas.remove("banana") - remove item selecionado da lista
+#frutas.pop() - remove qualquer elemento aleatorio
+#frutas.clear - limpa lista
+#print(len(frutas)) - quantidade de itens
+
+
+#ESSES SÃO UTILIZADOS EM TUPLES
+
+#tuples deixa repetir itens
+#print(dir(frutas))
+#for futa in frutas:print(futa) - deixa em ordem
+#frutas.clear - limpa lista
+#print(len(frutas)) - quantidade de itens
+#print(index(frutas)) - quantidade de um mesmo itens na index(?)
+#print(frutas.count("banana")) - fala a quantidades de um mesmo item em uma lista
+
+
+
+#frutas.clear - limpa lista
+#frutas[0] = "pera" - substitui a possição q escolher
+#for futa in frutas:print(futa)
+#frutas.insert(0, "pera") - adiciona na frente
+#frutas.append("sopa") - add  
+#frutas.sort --> classifica
+#so se usa se os tipos são iguas
+#frutas.reverse --> inverte
+
+print(frutas)
--- Cp2-tudo/teste.py
+++ Cp2-tudo/teste.py
@@ -1,13 +1,5 @@
-frutas = ("maça", "abacaxi", "morango", "banana")
+frutas = ["maça", "abacaxi", "morango", "banana"]
 
-frutas[0]
 
-for i in (0,1,2,3):
-    print(frutas[i])
-
-#frutas.sort --> classifica
-#so se usa se os tipos são iguas
-#frutas.reverse --> inverte
-cordenadas = [(1,2), (3,4), (5,6)]
 
 
