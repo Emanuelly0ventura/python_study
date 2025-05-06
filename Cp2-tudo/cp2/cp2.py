8f55bfb66e9db50c039dce7085ee79c398a9d9fe
manu <haru.noom0@gmail.com>
2025-05-06 16:05:56 -0300

    cabei a cp2 python

--- Cp2-tudo/cp2/cp2.py
+++ Cp2-tudo/cp2/cp2.py
@@ -1,12 +1,135 @@
+#Emanuelly Ventura | RM: 562339 | Turma:TDSPJ1
+
 import json
 import os
 
-#TROCAS AS COISA BENÇA!!!!!
+#variavel para salvar os dados
+usuarios = 'dados.json'
 
-#parte dos atributos para 
+#funções para validar e processar dados
 
-arquivos = 'dados.json'
+def carregarDados():
+    try: 
+        with open(usuarios, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+def salvarDados(dados):
+    with open(usuarios, 'w', encoding='utf-8') as f:
+        json.dump(dados, f, indent=4, ensure_ascii=False)
+
+#funções para as varieaveis de idade,
+
+def validarEstado(estado):
+    return estado.upper() in estados
+
+def validarIdade(idade):
+    if 0 <= idade <= 120:
+        return True
+    return False
+
+def validarSexo(sexo):
+    return sexo.upper() in ('M', 'F')
+
+def criarUsuario(dados):
+    
+    nome = input("Nome: ").strip()
+    if nome in dados:
+        print("Usuario já registrado")
+        return
+    
+#atribitos principal
+
+    try:
+        idade = int(input("Idade: "))
+        if not validarIdade:
+            return
+            print("Idade fora do intervalo ou incorreto!")
+    except ValueError:
+        print("Idade incorreta, tente novamente!")
+        
+
+    sexo = input("Sexo (M/F): ").strip().upper()
+    if not validarSexo(sexo):
+        print("Sexo incorreto, tente novamente!")
+        return
+
+    cidade = input("Cidade: ").strip()
+
+    estado = input("Estado(sigla): ").strip().upper()
+    if not validarEstado(estado):
+        print("Estado incorreto, tente novamente!")
+        return
+    
+#ligado ao salvar_dados
+    dados[nome] = {
+        "idade": idade,
+        "sexo": sexo,
+        "cidade": cidade,
+        "estado": estado
+    }
+    salvarDados(dados)
+    print(f"Registro de {nome} criado com sucesso.")
+
+#comandos de execução
+
+def lerDados(dados):
+    if not dados:
+        print("Nenhum dado registrado")
+        return
+    for nome, info in dados.items():
+        print(f"Nome: {nome}") 
+        print(f"Idade: {info['idade']}")
+        print(f"Sexo: {info['sexo']}")
+        print(f"Cidade: {info['cidade']}")
+        print(f"Estado: {info['estado']}. \n")
+
+def removerUsuario(dados):
+    nome = input("Digite o nome do usuario para deletar: ").strip()
+    if nome in dados:
+        del dados[nome]
+        salvarDados(dados)
+        print("Usuario removido com sucesso")
+    else:
+        print("Usuario não detectado")
+
+#atributo que repete pq é para alterar
+def alterarUsuario(dados):
+    nome = input("Digite o nome do usuario para altera-lo: ").strip()
+    if nome not in dados:
+        print("Usuario não encontrado!")
+        return
+
+    try:
+        idade = int(input("Nova idade: "))
+    except ValueError:
+        print("Idade incorreta, tente novamente!")
+        
+
+    sexo = input("Novo sexo (M/F): ").strip().upper()
+    if not validarSexo(sexo):
+        print("Sexo incorreto, tente novamento!")
+        
+
+    cidade = input("Nova cidade: ").strip()
+    estado = input("Novo estado (UF): ").strip().upper()
+    if not validarEstado(estado):
+        print("Estado incorreto, tente novamente!")
+
+    dados[nome].update({
+        "idade": idade,
+        "sexo": sexo,
+        "cidade": cidade,
+        "estado": estado
+    })
+    salvarDados(dados)
+    print("Registro alterado com sucesso")
+
+#estados  
+
 estados = (
+    
     'AC', 'AL', 'AP',
     'AM', 'BA', 'CE',
     'DF', 'ES', 'GO',
@@ -18,128 +141,27 @@ estados = (
     'SP', 'SE', 'TO'
 )
 
-def carregar_dados():
-    try: #para evitar bugs / r = reading
-        with open(arquivos, 'r', encoding='utf-8') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {}
-#TROCAS AS COISA BENÇA!!!!!
-def salvar_dados(dados):
-    with open(arquivos, 'w', encoding='utf-8') as f:
-        json.dump(dados, f, indent=4, ensure_ascii=False)
-
-def validar_estado(estado):
-    return estado.upper() in estados
-
-#aqui ele reconhece qual é o sexo selecionado
-def validar_sexo(sexo):
-    return sexo.upper() in ('M', 'F')
-
-def criar_usuario(dados):
-    nome = input("Nome: ").strip()
-    if nome in dados:
-        print("Pessoa já registrada")
+def listaEstado(dados):
+    estado = input("Digite o estado (sigla): ").strip().upper()
+    if not validarEstado(estado):
+        print("Estado incorreto, tente novamente!")
         return
     
-#atribito principal
-    try:
-        idade = int(input("Idade: "))
-    except ValueError:
-        print("Idade inválida")
-        return
-
-    sexo = input("Sexo (M/F): ").strip().upper()
-    if not validar_sexo(sexo):
-        print("Sexo inválido.")
-        return
-
-    cidade = input("Cidade: ").strip()
-
-    estado = input("Estado (UF): ").strip().upper()
-    if not validar_estado(estado):
-        print("Estado inválido")
-        return
-#ligado ao salvar_dados
-    dados[nome] = {
-        "idade": idade,
-        "sexo": sexo,
-        "cidade": cidade,
-        "estado": estado
-    }
-    salvar_dados(dados)
-    print("Registro concluido")
-
-#comandos de execução
-
-def ler_dados(dados):
-    if not dados:
-        print("Nenhum dado registrado")
-        return
-    for nome, info in dados.items():
-        print(f"Nome: {nome}, Idade: {info['idade']}, Sexo: {info['sexo']}, Cidade: {info['cidade']}, Estado: {info['estado']}.")
-
-def remover_usuario(dados):
-    nome = input("Digite o nome do usuario para deletar: ").strip
-    if nome not in dados:
-        del dados[nome]
-        salvar_dados(dados)
-        print("Usuario removido com sucesso")
-    else:
-        print("Usuario não reconhecido")
-
-def alterar_usuario(dados):
-    nome = input("Digite o nome do usuario para altera-lo")
-    if nome not in dados:
-        print("Usuario não encontrado")
-#TROCAS AS COISA BENÇA!!!!!
-    try:
-        idade = int(input("Nova idade: "))
-    except ValueError:
-        print("Idade inválida.")
-        
-
-    sexo = input("Novo sexo (M/F): ").strip().upper()
-    if not validar_sexo(sexo):
-        print("Sexo inválido.")
-        
-
-    cidade = input("Nova cidade: ").strip()
-    estado = input("Novo estado (UF): ").strip().upper()
-    if not validar_estado(estado):
-        print("Estado inválido.")
-
-    dados[nome].update({
-        "idade": idade,
-        "sexo": sexo,
-        "cidade": cidade,
-        "estado": estado
-    })
-    salvar_dados(dados)
-    print("Registro alterado com sucesso.")
-
-#menu de exibição 
-
-def lista_estado(dados):
-    estado = input("Digite o estado (UF): ").strip().upper()
-    if not validar_estado(estado):
-        print("Estado inválido.")
-        return
-
+# Cria uma lista de nomes das pessoas que estão no estado desejado
     encontrados = [nome for nome, info in dados.items() if info['estado'] == estado]
     if encontrados:
         print(f"Pessoas do estado {estado}:")
         for nome in encontrados:
             print(f"- {nome}")
     else:
-        print("Nenhuma pessoa encontrada nesse estado.")
-
-#TROCAS AS COISA BENÇA!!!!!
+        print(f"Não há pessoas cadastradas no estado {estado}.")
+    
+#menu principal 
 def menu():
-    dados = salvar_dados()
+    dados = carregarDados()
     while True:
-        print("\nMENU:")
-        print("1 - Ler e apresentar os dados do arquivo JSON")
+        print("\nMenu:")
+        print("1 - Ler e apresentar os dados")
         print("2 - Criar novos registros")
         print("3 - Remover registros")
         print("4 - Alterar (idade, sexo, cidade, estado) de uma pessoa")
@@ -147,30 +169,31 @@ def menu():
         print("6 - Sair do programa")
 
         try:
-            opcao = int(input("Escolha uma opção: "))
+            opcao = int(input("Escolha um numero: "))
         except ValueError:
-            print("Opção inválida.")
+            print("numero incorreto, tente novamente!")
             continue
 
         match opcao:
             case 1:
-                ler_dados(dados)
+                lerDados(dados)
             case 2:
-                criar_usuario(dados)
+                criarUsuario(dados)
             case 3:
-                remover_usuario(dados)
+                removerUsuario(dados)
             case 4:
-                alterar_usuario(dados)
+                alterarUsuario(dados)
             case 5:
-                lista_estado(dados)
+                listaEstado(dados)
             case 6:
-                print("Encerrando o programa.")
+                print("Sair")
                 break
             case _:
-                print("Opção inválida.")
-
+                print("Opção invalida")
+                
 if __name__ == "__main__":
     menu()
+
 
     
         
