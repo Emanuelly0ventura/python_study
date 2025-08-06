277ab138c849b65423dee90f50691e59c35b4bf0
manu <haru.noom0@gmail.com>
2025-08-05 21:01:36 -0300

    matris

---
+++ Cp3-tudo/matris.py
@@ -0,0 +1,12 @@
+# frutas = ["banana", "uva", "pera"]
+# vegetais = ["batata", "cenoura", "beterraba"]
+# carnes = ["boi", "peixe", "frango"]
+
+# mistura = [["banana", "uva", "pera"], ["batata", "cenoura", "beterraba"], ["boi", "peixe", "frango"]]
+
+# for grupo in mistura:
+#     for comida in grupo:
+#         print(comida, end=" ")
+
+#     print()
+
